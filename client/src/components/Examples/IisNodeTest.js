7681dd0119ee3091b637a0913b74f41065a4ecaa
Dan Farino <dan@sabio.la>
2018-07-17 21:00:02 -0700

    works with sample AJAX calls to .NET and Node.js backends

---
+++ client/src/components/Examples/IisNodeTest.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { getDotNet, getNode } from "../../services/exampleService";
+
+class IisNodeTest extends React.Component {
+  state = {
+    loading: true
+  };
+
+  componentDidMount() {
+    const promises = Promise.all([getDotNet(), getNode()]);
+    promises.then(([dotNetResponse, nodeResponse]) => {
+      this.setState({
+        dotNetResponse,
+        nodeResponse,
+        loading: false
+      });
+    });
+  }
+
+  render() {
+    const { loading, dotNetResponse, nodeResponse } = this.state;
+
+    return (
+      <div className="App">
+        <div>
+          <h1>Response from .NET</h1>
+          <pre>
+            {loading ? "Loading..." : JSON.stringify(dotNetResponse, null, 3)}
+          </pre>
+        </div>
+        <div>
+          <h1>Response from Node.js</h1>
+          <pre>
+            {loading ? "Loading..." : JSON.stringify(nodeResponse, null, 3)}
+          </pre>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default IisNodeTest;
---
+++ client/src/services/exampleService.js
@@ -0,0 +1,11 @@
+// sabio example service
+
+import axios from "axios";
+
+export function getDotNet() {
+  return axios.get("/api/test/dotnet");
+}
+
+export function getNode() {
+  return axios.get("/node-api/server.js/api/pogs");
+}
