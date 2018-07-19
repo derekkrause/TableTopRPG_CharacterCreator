e912072b5f54653610fb6b31d4cf86cbece3d798
ji kim <azenitha@gmail.com>
2018-07-18 17:04:21 -0700

    wip

---
+++ Sabio.Models/Domain/Blog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Data.Models
+{
+    public class BlogBase
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+        public string Slug { get; set; }
+        public string AuthorId { get; set; }
+        public DateTime PublishDate { get; set; }
+        public bool IsPublished { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
---
+++ Sabio.Services/BlogsService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Data.Services
+{
+    class BlogService
+    {
+        string connectionString = configurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+        public Sabio.Models.BlogViewModel GetAll()
+        {
+
+        }
+    }
+}
