3861931eafc6bb34c41dd659e34c6f4281e001f0
Derek Krause <krause96@gmail.com>
2018-08-01 10:11:30 -0700

    fixed webserver build

--- Sabio.Web/Controllers/Api/ExampleController.cs
+++ Sabio.Web/Controllers/Api/ExampleController.cs
@@ -1,9 +1,5 @@
 ﻿using Sabio.Models.Domain;
-using Sabio.Service;
 using Sabio.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
