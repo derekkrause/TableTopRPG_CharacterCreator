124d9db5fa5f6beccbb18fd3651b7222cc61715b
Dan Farino <dan@sabio.la>
2018-08-13 13:50:37 -0700

    Fix copy-client.cmd again

--- copy-client.cmd
+++ copy-client.cmd
@@ -1,4 +1,4 @@
 if not exist "Sabio.Web\Client" mkdir Sabio.Web\Client
 del /f /s /q Sabio.Web\Client\*.* > NUL
-xcopy /q /e client\build\*.* Sabio.Web\Client
+xcopy /q /e client\dist\*.* Sabio.Web\Client
 dir Sabio.Web\Client
