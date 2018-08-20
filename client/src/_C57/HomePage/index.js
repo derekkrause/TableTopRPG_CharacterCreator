cf5b828545cf71b7a6efedd9ef09c550105f0627
Derek Krause <krause96@gmail.com>
2018-08-20 15:05:49 -0700

    fixing conflicts

--- client/src/_C57/HomePage/index.js
+++ client/src/_C57/HomePage/index.js
@@ -25,49 +25,49 @@ class HomePage extends React.Component {
     coaches: []
   };
 
-  // componentDidMount() {
-  //   getUpcoming().then(res => {
-  //     console.log("events get all", res);
-  //     this.setState({
-  //       events: res.data.items
-  //     });
-  //   });
+  componentDidMount() {
+    getUpcoming().then(res => {
+      console.log("events get all", res);
+      this.setState({
+        events: res.data.items
+      });
+    });
 
-  //   getSchools(0).then(res => {
-  //     console.log("School get all", res);
-  //     this.setState({
-  //       schools: res.data.resultSets[0]
-  //     });
-  //   });
-  //   const sportType = "Baseball";
-  //   getAthleteTrend(sportType).then(res => {
-  //     console.log("GET TREND ATHLETE", res);
-  //     this.setState({
-  //       athletes: res.data.item.pagedItems
-  //     });
-  //   });
+    getSchools(0).then(res => {
+      console.log("School get all", res);
+      this.setState({
+        schools: res.data.resultSets[0]
+      });
+    });
+    const sportType = "Baseball";
+    getAthleteTrend(sportType).then(res => {
+      console.log("GET TREND ATHLETE", res);
+      this.setState({
+        athletes: res.data.item.pagedItems
+      });
+    });
 
-  //   getCoachTrend().then(res => {
-  //     const coaches = res.data.resultSets[0].map(o => {
-  //       var newObj = {};
-  //       for (const key of Object.keys(o)) {
-  //         const newKey = key[0].toLowerCase() + key.slice(1);
-  //         newObj[newKey] = o[key];
-  //       }
-  //       return newObj;
-  //     });
-  //     console.log("GET Coaches", res);
+    getCoachTrend().then(res => {
+      const coaches = res.data.resultSets[0].map(o => {
+        var newObj = {};
+        for (const key of Object.keys(o)) {
+          const newKey = key[0].toLowerCase() + key.slice(1);
+          newObj[newKey] = o[key];
+        }
+        return newObj;
+      });
+      console.log("GET Coaches", res);
 
-  //     this.setState(
-  //       {
-  //         coaches
-  //       },
-  //       () => {
-  //         console.log("After Coach Set state", this.state.coaches);
-  //       }
-  //     );
-  //   });
-  // }
+      this.setState(
+        {
+          coaches
+        },
+        () => {
+          console.log("After Coach Set state", this.state.coaches);
+        }
+      );
+    });
+  }
 
   render() {
     const { articleTitle, eventTitle, events, schools, athletes, coaches } = this.state;
