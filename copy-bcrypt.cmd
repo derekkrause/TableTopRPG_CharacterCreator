1ddbea3eca7e8380361462bed78a7f7e1f020089
Dan Farino <dan@sabio.la>
2018-09-05 10:24:22 -0700

    fix broken copy command

--- copy-bcrypt.cmd
+++ copy-bcrypt.cmd
@@ -1,1 +1,1 @@
-xcopy /q Sabio.Web\node-api\bcrypt_lib.node Sabio.Web\node-api\node_modules\bcrypt\lib\binding
+xcopy /q /Y Sabio.Web\node-api\bcrypt_lib.node Sabio.Web\node-api\node_modules\bcrypt\lib\binding
